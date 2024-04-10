4bffaf2968eecef520588ff5345025f2aca44418
IB64 <iadbeleno@gmail.com>
2024-04-10 15:44:16 +0100

    changes done

--- generate_ids.py
+++ generate_ids.py
@@ -9,6 +9,30 @@ from requests.exceptions import Timeout, HTTPError
 TIMEOUT = 20
 START_RANGE = 76561197000000000
 
+def get_valid_random_ids(steam_ids: str) -> list:
+    """
+    From the randomly generated steam ids, check to see which ones are actually valid.
+    """
+    url = f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={environ['STEAM_API_KEY']}&steamids={steam_ids}"
+    try:
+        response = requests.get(url, timeout=TIMEOUT)
+    except ConnectionError as exc:
+        raise ConnectionError("Connection failed") from exc
+    except Timeout as exc:
+        raise Timeout("The request timed out.") from exc
+    except HTTPError as exc:
+        raise HTTPError("url is invalid.") from exc
+    
+    info = response.json()
+
+    if not info["response"]["players"]:
+        return None
+    
+    result = []
+    for player in info["response"]["players"]:
+        result.append(player["steamid"])
+    return result
+
 
 def valid_steam_id(steam_id: str) -> bool:
     """
@@ -39,11 +63,21 @@ def valid_steam_id(steam_id: str) -> bool:
     return True
 
 
-def generate_random_id() -> str:
+def generate_random_ids() -> list:
     """
-    Generates a potential Steam ID.
+    Generates 100 random potential Steam ID.
     """
-    return random.randint(START_RANGE, 76561199999999999)
+    return [str(random.randint(START_RANGE, 76561199999999999)) for _ in range(100)]
+
+
+def convert_list_to_string(steam_ids: list) -> str:
+    """
+    Converts the list of random steam ids into a comma separated string.
+    """
+    string = steam_ids[0]
+    for id in steam_ids[1:]:
+        string = string + "," + id
+    return string
 
 
 def generate_valid_steam_ids(number_to_generate: int) -> None:
@@ -54,14 +88,22 @@ def generate_valid_steam_ids(number_to_generate: int) -> None:
     counter = 0
     steam_ids = []
     while counter != number_to_generate:
-        random_id = generate_random_id()
-        if valid_steam_id(random_id):
-            steam_ids.append(str(random_id))
-            counter += 1
-            print(f"Valid Id number: {counter} found...")
+        random_ids = generate_random_ids()
+        valid_ids = get_valid_random_ids(random_ids)
+        while not valid_ids:
+            valid_ids = get_valid_random_ids(random_ids)
+
+        for id in valid_ids:
+            if valid_steam_id(id):
+                steam_ids.append(str(id))
+                counter += 1
+                print(f"ID: {id} is valid. {counter}/{number_to_generate} found...")
 
     file = open("steam_ids.txt", "w")
     for id in steam_ids:
         file.write(id + "\n")
     file.close()
     print("Id generation done! Time Elapsed: ", perf_counter() - start)
+
+if __name__ == "__main__":
+    generate_valid_steam_ids(100)
