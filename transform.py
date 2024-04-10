70ed34080c4ec5ab04922b27ed5ab218821371af
IB64 <iadbeleno@gmail.com>
2024-04-10 13:13:59 +0100

    did 100 random people

--- transform.py
+++ transform.py
@@ -13,4 +13,6 @@ def clean_data(data: pd.DataFrame) -> pd.DataFrame:
     Clean the data to remove any weird values.
     """
     data = data.drop(data[data["Game_Genres"] == "\n\nUnder £7\n\n\nUnder £4\n\n"].index)
+    data = data.drop(data[data["Game_Genres"] == "NULL"].index)
+    data = data.loc[~((data["Playtime_Minutes"] == 0))].reset_index(drop=True)
     return data
